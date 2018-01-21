516913a66c5ec0d85e5deaa817f24c922826cd04
Sudheer Satyanarayana <sudheer.zzz@sudheer.net>
2018-01-21 13:34:12 +0530

    Molecule 2.x test

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,23 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+def test_ruby_24_installed(host):
+    ruby = host.package("rh-ruby24")
+    assert ruby.is_installed
+    assert ruby.version == "2.4"
+
+def test_ruby_24_available(host):
+    ruby = host.check_output('source /etc/profile.d/enableruby24.sh && which ruby')
+    assert ruby == '/opt/rh/rh-ruby24/root/usr/bin/ruby'
